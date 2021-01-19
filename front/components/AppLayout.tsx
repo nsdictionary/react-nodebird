9312ad4e74020427718ad1068b00fa3ab205f0c0
sam <3161728@gmail.com>
2021-01-19 15:31:06 +0900

    Test antd grid

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 interface IProps {
   children: React.ReactNode;
@@ -21,12 +21,37 @@ const AppLayout = ({ children }: IProps) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>Signup</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col
+          style={{ backgroundColor: "blue", height: "100px" }}
+          xs={24}
+          md={6}
+        />
+        <Col
+          style={{ backgroundColor: "red", height: "100px" }}
+          xs={24}
+          md={12}
+        >
+          {children}
+        </Col>
+        <Col
+          style={{ backgroundColor: "green", height: "100px" }}
+          xs={24}
+          md={6}
+        >
+          <a href="http://google.com" target="_blank" rel="noreferrer noopener">
+            Google
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
