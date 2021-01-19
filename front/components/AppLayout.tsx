1ddec558f2e087d48454a5781074d9391e362368
sam <3161728@gmail.com>
2021-01-19 14:48:43 +0900

    Install antd and styled components

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { Menu } from "antd";
 
 interface IProps {
   children: React.ReactNode;
@@ -8,17 +9,23 @@ interface IProps {
 const AppLayout = ({ children }: IProps) => {
   return (
     <div>
-      <div>
-        <Link href="/">
-          <a>Nodebird</a>
-        </Link>
-        <Link href="/profile">
-          <a>Profile</a>
-        </Link>
-        <Link href="/signup">
-          <a>Signup</a>
-        </Link>
-      </div>
+      <Menu mode={"horizontal"}>
+        <Menu.Item>
+          <Link href="/">
+            <a>Nodebird</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>Profile</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>Signup</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
       {children}
     </div>
   );
