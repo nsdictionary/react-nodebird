2d27507be8af244a73a27bef22a15797dee04438
sam <3161728@gmail.com>
2021-01-21 21:00:36 +0900

    Fix gutter css layout bug

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -6,20 +6,38 @@ import LoginForm from "./LoginForm";
 import UserProfile from "./UserProfile";
 import styled from "styled-components";
 import { IState } from "../reducers";
+import { createGlobalStyle } from "styled-components";
 
-interface IProps {
-  children: React.ReactNode;
-}
+// antd design bug fix (gutter)
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+  
+  .ant-col:first-child {
+    padding-left: 0 !important;
+  }
+
+  .ant-col:last-child {
+    padding-left: 0 !important;
+  }
+`;
 
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `;
+
+interface IProps {
+  children: React.ReactNode;
+}
 
 const AppLayout = ({ children }: IProps) => {
   const { isLoggedIn } = useSelector((state: IState) => state.user);
 
   return (
     <div>
+      <Global />
       <Menu mode={"horizontal"}>
         <Menu.Item>
           <Link href="/">
