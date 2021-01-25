5f3347b11ff9e672c7c67b5ac07c5d834a64ec49
sam <3161728@gmail.com>
2021-01-26 01:59:27 +0900

    Add progress ui

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -3,18 +3,28 @@ import "antd/dist/antd.css";
 import Head from "next/head";
 import wrapper from "../store/configureStore";
 
+import Router from "next/router";
+import NProgress from "nprogress"; //nprogress module
+import "nprogress/nprogress.css"; //styles of nprogress
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 interface IProps {
   Component: React.ComponentType;
+  pageProps: any;
 }
 
-const App = ({ Component }: IProps) => {
+const App = ({ Component, pageProps }: IProps) => {
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <title>NodeBird</title>
       </Head>
-      <Component />
+      <Component {...pageProps} />
     </>
   );
 };
